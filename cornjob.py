d0892f92c80549ab2db6a40f6668e761281efd6d
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2023-08-30 20:22:32 +0530

    cornjobt_est

--- cornjob.py
+++ cornjob.py
@@ -1,19 +1,19 @@
 import time
 import subprocess
 
-# Define the time at which you want the script to run daily (24-hour format)
-# For example, 00:00 (midnight) in this case
-desired_time = "00:00"
+
+
+desired_time = "20:17"
 
 while True:
-    # Get the current time
+    #current time
     current_time = time.strftime("%H:%M")
 
-    # Check if the current time matches the desired time
+
     if current_time == desired_time:
         try:
-            # Run your script using subprocess
-            subprocess.run(["/usr/bin/python3", r"C:\Users\abhis\PycharmProjects\ultimateScrapper\runner.py"])
+
+            subprocess.run(["python", "runner.py"])
         except Exception as e:
             print(f"An error occurred: {str(e)}")
 
