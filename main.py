80a486fffc204d266dd5bfe394293fc4f02661b6
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2023-08-30 13:09:01 +0530

    added headless on driver option

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-
+chrome_options.add_argument("--headless")
 # Set the viewport size
 chrome_options.add_argument("--window-size=1920,1080")
 
