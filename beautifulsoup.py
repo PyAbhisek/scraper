53e4470d8c97e790807f03245765b0826aefc97c
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2023-10-03 17:31:29 +0530

    updated the code for nykaa_brands

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,27 +1,61 @@
 import json
 import requests
-import time
+from io import BytesIO
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, ElementNotInteractableException ,ElementClickInterceptedException
-from bs4 import BeautifulSoup
-from PIL import Image
-from io import BytesIO
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, \
+    ElementNotInteractableException, ElementClickInterceptedException
+from bs4 import BeautifulSoup
+import time
+from PIL import Image
+import boto3
+import traceback
+
+import os  # Import the os module
+from datetime import date
+
+# Get the current date in the format "dd-mm-yyyy"
+current_date = date.today().strftime("%d-%m-%Y")
+
+# Create a folder with the current date if it doesn't exist
+folder_name = os.path.join(os.getcwd(), current_date)
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+# Define the local directory containing the files to upload
+output_directory = folder_name
+
+# Define the log file path
+log_file_path = os.path.join(output_directory, 'scraping_log.txt')
 
 # Load configuration from config.json
 with open('BS4Config.json') as f:
     scraping_targets = json.load(f)
 
-chrome_driver_path = r'C:\Users\abhis\OneDrive\Desktop\pythonselenium\chromedriver-win64\chromedriver.exe'
-chrome_service = Service(chrome_driver_path)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument(
+    "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+
+# Set the viewport size
+chrome_options.add_argument("--window-size=1920,1080")
+
+# Path to ChromeDriver executable
+chrome_driver_path = '/usr/bin/chromedriver'
+
+# Initialize counters
+total_product_count = 0
+website_product_counts = {}
 
 def get_image_rgb(url):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
     response = requests.get(url, headers=headers)
     img = Image.open(BytesIO(response.content))
     width, height = img.size
@@ -34,190 +68,238 @@ def click_element_safely(element):
     except StaleElementReferenceException:
         # Handle the StaleElementReferenceException by re-locating the element before clicking
         click_element_safely(element)
-def handle_popups(driver):
-    try:
-        # Define the XPath for the popup and close button
-        popup_xpath = '//*[@id="bik-widget-wrapper-Q0bQgojHaEBAsabJV5Xp"]/div/div'
-        close_button_xpath = '//*[@id="bik-widget-wrapper-Q0bQgojHaEBAsabJV5Xp"]/div/div/div[1]/div'
 
-        # Wait for the popup to become visible (timeout after 10 seconds)
-        wait = WebDriverWait(driver, 10)
-        popup = wait.until(EC.visibility_of_element_located((By.XPATH, popup_xpath)))
-
-        if popup.is_displayed():
-            # Handle the pop-up here
-            # For example, you can click a close button to dismiss it
-            close_button = popup.find_element(By.XPATH, close_button_xpath)
-            close_button.click()
-    except NoSuchElementException:
-        # No pop-up found, continue with scraping
-        pass
-all_data = []
+# Initialize a dictionary to store data for each website
+data_by_website = {}
 
 for target in scraping_targets:
+    filename_prefix = target["filename"]
+    # Check if the website is already in the dictionary
+    if filename_prefix not in data_by_website:
+        data_by_website[filename_prefix] = []
+
     product_name = target["product_name"]
     shade_image_srcset = target["shade_image_srcset"]
     shade_click = target["shade_click"]
     shade_Original_Price = target['shade_Original_Price']
     shade_price = target['shade_price']
-    shade_name = target['shade_name']
+    shade_name_tag = target['shade_name_tag']
     image_base_url = target['image_base_url']
     vendor = target['vendor']
-    filename_prefix = target["filename"]
     shade_image_src = target["shade_image_src"]
     parent_element = target['parent_element']
     product_name_parent = target['product_name_parent']
     price_parent = target['price_parent']
     img_tag = target['img_tag']
     img_parent = target['img_parent']
+    shade_details_parent = target['shade_details_parent']
 
+    # Load links from JSON file
+    with open(f"{filename_prefix}.json", 'r') as f:
+        links_data = json.load(f)
 
-    def scrape_data(url):
-        driver = webdriver.Chrome(service=chrome_service)
+    links = links_data['links']
+
+    # Count the shades for each website
+    total_shades_count = 0
+
+    # Store the scraped data for a website
+    website_data = {
+        "website_name": filename_prefix,
+        "shades": []
+    }
+
+    for url in links:
+        print(f"Scraping data from: {url}")
+        driver = webdriver.Chrome(options=chrome_options)
         driver.get(url)
 
         try:
             driver.implicitly_wait(10)
-            product_counter = 0  # Initialize the product_counter
-            trigger_popup_after = 2  # Set the number of iterations after which the popup should be triggered
+            try:
+                if parent_element:
+                    parent = driver.find_element(By.CSS_SELECTOR, parent_element)
+                    shade_items = parent.find_elements(By.CSS_SELECTOR, shade_click)
+                else:
+                    shade_items = driver.find_elements(By.CSS_SELECTOR, shade_click)
+            except NoSuchElementException:
+                print("No Such Element")
+                continue
 
-            if parent_element:
-                parent = driver.find_element(By.CSS_SELECTOR, parent_element)
-                shade_items = parent.find_elements(By.CSS_SELECTOR, shade_click)
-            else:
-                shade_items = driver.find_elements(By.CSS_SELECTOR, shade_click)
-
-            product_data = {
-                "url": url,
-                "shades": []
-            }
+            product_counter = 0
 
             for shade_item in shade_items:
-                product_counter += 1  # Increment the product_counter for each iteration
+                driver.implicitly_wait(10)
+                product_counter += 1
+                total_shades_count += 1  # Increment the total shades count for this website
                 print(f"Product #{product_counter}")
 
                 try:
                     shade_item.click()
-                except (ElementNotInteractableException, ElementClickInterceptedException):
+                except ElementNotInteractableException:
                     print("Could not click on the shade item (OUT OF STOCK). Skipping to the next one.")
-                    continue
+                except ElementClickInterceptedException:
+                    print("Element is click-intercepted, waiting for a second and trying again.")
+                    time.sleep(1)  # Wait for a second
+                    try:
+                        shade_item.click()  # Attempt to click again after waiting
+                    except (ElementNotInteractableException, ElementClickInterceptedException):
+                        print("Still unable to click on the shade item. Skipping to the next one.")
 
-                time.sleep(3)
+                # time.sleep(3)
                 window_location = driver.execute_script("return window.location.href")
                 print(window_location)
+                page_source = driver.page_source
+                soup = BeautifulSoup(page_source, 'html.parser')
 
-                if filename_prefix == "typsybeauty" and product_counter == trigger_popup_after:
-                    handle_popups(driver)
-
-                # for shade price
+                # saleprice
                 try:
                     price_match = driver.find_element(By.CSS_SELECTOR, shade_price)
-                    # price = price_match.text.strip()
                     price = price_match.text.strip()
-                    if not price:
-                        price = price_match.get_attribute("textContent").strip()
-                    if not price:
-                        price = price_match.get_attribute("innerText").strip()
-
                     print(price)
-
                 except NoSuchElementException:
                     price = "Price not found"
                     print(price)
 
-                # for original shade price
-                oprice = ""
-                if price_parent:
-                    try:
-                        parent_price = driver.find_element(By.CSS_SELECTOR, price_parent)
-                        originalprice_match = parent_price.find_element(By.CSS_SELECTOR, shade_Original_Price)
-                        oprice = originalprice_match.text.strip()
-                        print(oprice)
-                    except NoSuchElementException:
-                        oprice = "Price not found"
-                        print(oprice)
+                # originalprice
+                try:
+                    original_price_match = driver.find_element(By.CSS_SELECTOR, shade_Original_Price)
+                    original_price = original_price_match.text.strip()
+                    print(original_price)
+                except NoSuchElementException:
+                    original_price = "Price not found"
+                    print(original_price)
 
                 # for product name
+                result = None
                 try:
-                    if product_name_parent:
-                        product_name_parent_container = driver.find_element(By.CSS_SELECTOR, product_name_parent)
-                        name_match = product_name_parent_container.find_element(By.CSS_SELECTOR, product_name)
+                    name_match = driver.find_element(By.CSS_SELECTOR, product_name)
+                    pname = name_match.text.strip()
+                    print(pname)
+                    keywords = ['satin', 'metallic', 'matte', 'gloss']
+                    # Iterate through keywords and check if they are in the product name
+                    found_keywords = [keyword for keyword in keywords if keyword in pname.lower()]
+                    if found_keywords:
+                        # Print the first found keyword
+                        result = found_keywords[0]
+                        print(result)
                     else:
-                        name_match = driver.find_element(By.CSS_SELECTOR, product_name)
-
-                    productname = name_match.text.strip()
+                        result = "N/A"
+                        print(result)
                 except NoSuchElementException:
-                    productname = "Product name not found"
-                print(productname)
+                    pname = "Product name not found"
+                    print(pname)
 
-                # for shade name
-
+                # shadename
                 try:
-                    test = driver.find_element(By.CSS_SELECTOR, shade_name)
-                    selected_shade_text = test.text.strip()
-                    print(selected_shade_text)
+                    test = driver.find_element(By.CSS_SELECTOR, shade_details_parent)
+                    shade_name = test.find_element(By.TAG_NAME, shade_name_tag)
+                    selected_shade_text = shade_name.get_attribute('alt')
+                    selected_shade_name = selected_shade_text.replace('Color:', '').strip()
+                    print(selected_shade_name)
+
                 except NoSuchElementException:
-                    selected_shade_text = "Shade name not found"
-                    print(selected_shade_text)
+                    selected_shade_name = "Shade name not found"
+                    print(selected_shade_name)
 
-
-                # for image
+                # shadecolor
                 try:
+                    color_code_img = driver.find_element(By.CSS_SELECTOR, shade_details_parent)
+                    shade_name = color_code_img.find_element(By.TAG_NAME, shade_name_tag)
+                    color_code = shade_name.get_attribute('src')
+                    background_color_rgb = get_image_rgb(color_code)
+                    print(background_color_rgb)
 
-                    if shade_image_srcset:
-                        test1 = driver.find_element(By.XPATH, shade_image_srcset)
-                        data = test1.get_attribute('srcset')
-                        data = data.replace('//', '')
-                    elif shade_image_src:
-                        test1 = driver.find_element(By.XPATH, shade_image_src)
-                        data = test1.get_attribute('src')
-                    else:
-                        # Check if the image tag is present and get the srcset attribute if available
-                        parent_img = driver.find_element(By.CSS_SELECTOR, img_parent)
-                        image_tag = parent_img.find_element(By.CSS_SELECTOR, img_tag)
-                        data = image_tag.get_attribute('srcset') if image_tag else None
-                        data = data.replace('//', '')
-                    # print(data)
-                    if data.startswith('/'):
-                        full_image_url = image_base_url + data
-                    else:
-                        full_image_url = data.split()[0] if data else "Image URL not available"
+                except NoSuchElementException:
+                    background_color_rgb = "color code not found"
+                    print(background_color_rgb)
+
+                # productimage
+                try:
+                    test1 = driver.find_element(By.CSS_SELECTOR,
+                                                '#app > div.css-e82s8r > div.css-16kpx0l > div.css-14y2xde > div.css-1mruek6 > div.css-ov1ktg > div.productSelectedImage.css-eyk94w > div > div > img')
+                    data = test1.get_attribute('src')
+                    data = data.replace('//', '')
+                    full_image_url = data.split()[0] if data else "Image URL not available"
                     print(full_image_url)
                 except NoSuchElementException:
                     full_image_url = "Image URL not available"
                     print(full_image_url)
 
-                if "combo" not in selected_shade_text.lower():
-                    shade_data = {
-                        "shade_url": window_location,
-                        "shade_image_url": full_image_url,
-                        "shade_price": price,
-                        "Product_Name": productname,
-                        "original_price": oprice,
-                        "Selected_shade_name": selected_shade_text,
-                        "vendor": vendor
-                    }
+                shade_data = {
+                    "shade_url": window_location,
+                    "shade_color": background_color_rgb,
+                    "shade_image_url": full_image_url,
+                    "shade_sale_price": price,
+                    "shade_original_price": original_price,
+                    "Product_Name": pname,
+                    "product_type": result,
+                    "Selected_shade_name": selected_shade_name,
+                    "vendor": vendor
+                }
 
-                    product_data["shades"].append(shade_data)
+                # Append the scraped data to the website's data
+                website_data["shades"].append(shade_data)
 
-            return product_data
+            # Print the total count of shades for this website
+            print(f"Total shades for {filename_prefix}: {total_shades_count}")
+
+            # Update the total product count and website product count
+            total_product_count += total_shades_count
+            website_product_counts[filename_prefix] = total_shades_count
+
+        except Exception as e:
+            print(f"An error occurred while scraping the URL: {url}")
+            print(f"Error: {str(e)}")
+            with open(log_file_path, 'a') as log_file:
+                log_file.write(f"An error occurred while scraping the URL: {url}\n")
+                log_file.write(f"Error: {str(e)}\n")
+                traceback.print_exc()
+            # If an error occurs, continue to indicate that the scraping failed for this URL
+            continue
 
         finally:
             driver.quit()
 
-    with open(f"{filename_prefix}_links.json", 'r') as f:
-        links_data = json.load(f)
+    # Save scraped data for this website to a JSON file
+    output_file = os.path.join(output_directory, f"{filename_prefix}.json")
+    with open(output_file, 'w') as f:
+        json.dump(website_data, f, indent=4)
+    print(f"Scraped data for {filename_prefix} saved to '{output_file}'")
 
-    links = links_data['links']
+# Print the total count of shades across all websites
+print(f"Total shades across all websites: {total_product_count}")
 
-    for url in links:
-        print(f"Scraping data from: {url}")
-        scraped_data = scrape_data(url)
-        all_data.append(scraped_data)
-        print("-" * 50)
 
-output_file = 'scraped_data.json'
-with open(output_file, 'w') as f:
-    json.dump(all_data, f, indent=4)
+# Print the counts for each website
+for website, count in website_product_counts.items():
+    print(f"Total shades for {website}: {count}")
 
-print(f"Scraped data saved to '{output_file}'")
+print(f"Scraped data saved in directory '{output_directory}'")
+
+
+# AWS credentials and S3 bucket information
+aws_access_key_id = 'AKIA5LN5QZFXC7TK5BXL'
+aws_secret_access_key = '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'
+bucket_name = 'dataset-image-dev'
+s3_prefix = 'web_scrape_data/' + current_date + '/' + 'nykaa_Setup' + '/'
+
+# Create an S3 client
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+# Iterate through the files in the output_directory and upload them to S3
+for root, dirs, files in os.walk(output_directory):
+    for file in files:
+        local_path = os.path.join(root, file)
+        # Define the S3 key (object name) by removing the local directory path
+        s3_key = os.path.join(s3_prefix, os.path.relpath(local_path, output_directory))
+
+        try:
+            # Upload the file to S3
+            s3.upload_file(local_path, bucket_name, s3_key)
+            print(f"Uploaded {local_path} to s3://{bucket_name}/{s3_key}")
+        except Exception as e:
+            print(f"Error uploading {local_path} to s3://{bucket_name}/{s3_key}: {e}")
+
+print(f"Scraped data in '{output_directory}' uploaded to S3 bucket '{bucket_name}' with prefix '{s3_prefix}'")
+
--- main.py
+++ main.py
@@ -1,89 +1,110 @@
 import json
 from selenium import webdriver
-from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 # Load configuration from config.json
-with open('config.json') as f:
-    scraping_targets = json.load(f)
+with open("config.json", "r") as config_file:
+    config_data = json.load(config_file)
 
-chrome_driver_path = r'C:\Users\abhis\OneDrive\Desktop\pythonselenium\chromedriver-win64\chromedriver.exe'
-chrome_service = Service(chrome_driver_path)
+# Keywords to exclude
+exclude_keywords = ['lip-juicers', '-in-1', 'kajal', 'casing', 'lip-gloss', 'brush', 'concealer', 'changing',
+                   'refillable-case', 'pencil', 'lip-liner', 'lipliner', 'lip-plumper', 'lip-oil', 'multi-mousse',
+                   'lip-balm', 'lip-care', 'cheek-stain', 'cheek', 'scrub', 'crayon', 'pack', 'combo', 'fab5',
+                   'lacquer', 'mini', 'duo', '-for-1', 'trio', 'combo', 'set', 'tint',
+                   'kit','collection']
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument(
+    "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
 
-driver = webdriver.Chrome(service=chrome_service)
+# Set the viewport size
+chrome_options.add_argument("--window-size=1920,1080")
 
-for target in scraping_targets:
-    url = target["url"]
-    class_name = target["css_selector"]
-    filename_prefix = target["filename"]
-    loadmore = target['loadmore']
-    base_url = target['base_url']
-    startingrange = target['startingrange']
-    endingrange = target['endingrange']
-    parent_element = target.get('parent_element', '')  # Modified to use get method
-    wait = WebDriverWait(driver, 40)
+# Path to ChromeDriver executable
+chrome_driver_path = '/usr/bin/chromedriver'
 
-    driver.get(url)
+# Loop through each configuration
+for config in config_data:
+    url = config.get("url")
+    css_selector = config.get("css_selector")
+    filename = config.get("filename")
+    startingrange_str = config.get("startingrange")
+    endingrange_str = config.get("endingrange")
 
-    if loadmore:
-        try:
-            pagination_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, loadmore)))
-        except TimeoutException:
-            print(f"TimeoutException: Pagination button not found for URL: {url}")
-            continue
+    if startingrange_str and endingrange_str:
+        startingrange = int(startingrange_str)
+        endingrange = int(endingrange_str)
+        url1 = config.get("first_half_url")
+        url2 = config.get("second_half_url")
 
-        # Attempt to click the pagination button, retrying once more if it fails
-        try:
-            pagination_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, loadmore)))
-            pagination_button.click()
-        except StaleElementReferenceException:
-            print("StaleElementReferenceException: Retrying click on pagination button")
-            pagination_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, loadmore)))
-            pagination_button.click()
+        # Use a list to temporarily store data
+        links = []
 
-    try:
-        if parent_element:
-            parent_css = driver.find_element(By.CSS_SELECTOR, parent_element)
-            cards = parent_css.find_elements(By.CSS_SELECTOR, class_name)  # Find cards within the parent element
-        else:
-            cards = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, class_name)))
-    except TimeoutException:
-        print(f"TimeoutException: No cards found for URL: {url}")
-        continue
+        for page_number in range(startingrange, endingrange):
+            url = url1 + str(page_number) + url2
+            print(url)
 
-    # Use a set to temporarily store links and remove duplicates
-    links_set = set()
+            # Create a new WebDriver instance and navigate to the URL
+            driver = webdriver.Chrome(options=chrome_options)
+            driver.get(url)
 
-    # Keywords to exclude
-    exclude_keywords = ['lip-juicers','-in-1', 'kajal' ,'casing','lip-gloss', 'brush', 'concealer', 'changing', 'refillable-case', 'pencil',
-                        'lip-liner','lipliner', 'lip-plumper', 'lip-oil', 'multi-mousse', 'lip-balm', 'lip-care', 'cheek-stain', 'cheek' ,'scrub',
-                        'crayon' ,'pack', 'combo','fab5' ,'lacquer', 'mini' ,'duo', '-for-1' ,'trio']
-    for card in cards:
-        try:
+            wait = WebDriverWait(driver, 10)
+
+            # Find elements based on the provided CSS selector
+            cards = driver.find_elements(By.CSS_SELECTOR, css_selector)
+
+            # # Use a list to temporarily store data
+            # links = []
+
+            # Loop through the found elements
+            for card in cards:
+                href = card.get_attribute("href")
+
+                # Check if any of the exclude keywords are present in the href
+                if not any(keyword in href.lower() for keyword in exclude_keywords):
+                    links.append(href)  # Only add the link if it doesn't contain exclude keywords
+
+            driver.quit()
+
+            # Step 1: Create a dictionary to store the data
+            data = {"links": links}
+
+            # Step 2: Save the data dictionary to a JSON file with the provided filename
+            with open(f"{filename}.json", "w") as json_file:
+                json.dump(data, json_file)  # The 'indent' argument is optional for pretty formatting
+
+    else:
+        # Create a new WebDriver instance and navigate to the URL
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(url)
+
+        wait = WebDriverWait(driver, 10)
+
+        # Find elements based on the provided CSS selector
+        cards = driver.find_elements(By.CSS_SELECTOR, css_selector)
+
+        # Use a list to temporarily store data
+        links = []
+
+        # Loop through the found elements
+        for card in cards:
             href = card.get_attribute("href")
-            if href is not None and not any(keyword in href.lower() for keyword in exclude_keywords):
-                if href.startswith("/"):
-                    href = base_url + href  # Concatenate base_url and relative href
-                links_set.add(href)
-        except StaleElementReferenceException:
-            # Handle stale element reference here, you can choose to ignore it or refresh the element
-            print("StaleElementReferenceException: Handling stale element reference")
-            continue
 
-    # Convert the set to a list
-    links = list(links_set)
+            # Check if any of the exclude keywords are present in the href
+            if not any(keyword in href.lower() for keyword in exclude_keywords):
+                links.append(href)  # Only add the link if it doesn't contain exclude keywords
 
-    # Step 1: Create a dictionary to store the links
-    data = {"links": links}
+        driver.quit()
 
-    # Step 2: Save the links dictionary to a JSON file
-    output_filename = f"{filename_prefix}_links.json"
-    with open(output_filename, "w") as json_file:
-        json.dump(data, json_file)
+        # Step 1: Create a dictionary to store the data
+        data = {"links": links}
 
-driver.quit()
+        # Step 2: Save the data dictionary to a JSON file with the provided filename
+        with open(f"{filename}.json", "w") as json_file:
+            json.dump(data, json_file)  # The 'indent' argument is optional for pretty formatting
