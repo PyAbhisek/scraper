b0f50cefd2ccb9e1905ca145d3313c6819dce635
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2023-12-02 19:20:02 +0530

    updated the page numbers

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ chrome_options.add_argument("--window-size=1920,1080")
 
 # Path to ChromeDriver executable
 chrome_driver_path = '/usr/bin/chromedriver'
-# Initialize an empty list to store the links
-links = []
+# Initialize an empty set to store the links
+links_set = set()
 
 # Keywords to exclude
 exclude_keywords = ['lip-juicers', '-in-1', 'kajal', 'casing', 'lip-gloss', 'brush', 'concealer', 'changing',
                     'refillable-case', 'pencil', 'lip-liner', 'lipliner', 'lip-plumper', 'lip-oil',
                     'multi-mousse', 'lip-balm', 'lip-care', 'cheek-stain', 'cheek', 'scrub',
-                    'crayon', 'pack', 'combo', 'fab5', 'lacquer', 'mini']
+                    'crayon', 'pack', 'combo', 'fab5', 'lacquer', 'mini','kit','box']
 
-for page_number in range(1, 54):
+for page_number in range(1, 44):
     driver = webdriver.Chrome(options=chrome_options)
     website = f'https://www.nykaa.com/makeup/lips/lipstick/c/249?page_no={page_number}&sort=popularity&eq=desktop'
 
@@ -40,13 +40,13 @@ for page_number in range(1, 54):
         if any(keyword in href for keyword in exclude_keywords):
             continue  # Skip this link
 
-        links.append(href)
+        links_set.add(href)
 
     driver.quit()
 
-# Create a dictionary to store the links
-data = {"links": links}
+# Create a dictionary to store the links set
+data = {"links": list(links_set)}
 
-# Save the links dictionary to a JSON file
+# Save the links set to a JSON file
 with open("links.json", "w") as json_file:
-    json.dump(data, json_file, indent=4)  # The 'indent' argument is optional for pretty formatting
+    json.dump(data, json_file, indent=4)  
